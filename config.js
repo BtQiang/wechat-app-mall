067250e4157723bb0b742d3c584b372514d53eed
gooking <javazj@qq.com>
2026-05-05 11:43:01 +0800

    fixed:小程序客服vant无法拉起客服的bug

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  version: '26.4.27',
-  note: 'fixed:首页爆款推荐更多点击无反应', // 这个为版本描述，无需修改
+  version: '26.5.5',
+  note: 'fixed:小程序客服vant无法拉起客服的bug', // 这个为版本描述，无需修改
   subDomain: 'tz', // 此处改成你自己的专属域名。什么是专属域名？请看教程 https://www.it120.cc/help/qr6l4m.html
   merchantId: 951, // 商户ID，可在后台工厂设置-->商户信息查看
   sdkAppID: 1400450467, // 腾讯实时音视频应用编号，请看教程 https://www.it120.cc/help/nxoqsl.html
